9da2b7ae3f00f95b5635f135d5a43cb21357675b
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-06-07 16:51:42 +0530

    Created Nav menu for desktop screens - work is still pending. Nav menu dropdown animation will be worked on next.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("Working fine");
+// NAV BAR FUNCTION - SCROLL UP TO HIDE & SCROLL DOWN TO SHOW
+var lastScrollTop = 0;
+var navBar = document.getElementById("navigation-bar");
+
+window.addEventListener("scroll", function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){
+        navBar.style.top="-8.7rem";
+    }
+    else{
+        navBar.style.top="0";
+    }
+    lastScrollTop = scrollTop;
+})
