f6b4c0fafaf57729338138956d8e6a1aaf89b73c
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-06-09 19:11:48 +0530

    Nav Bar for large displays completed, all final touches added. Next for small display, side bar will be created. Changed the names of many components to make a structured approach.

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-// G-Drive CAR IMAGE LINKS
-// var nsx = "http://drive.google.com/uc?export=view&id=1rgWolMWhuHaB0XVp3kPhBD6X0Bmk8NQj";
-// var rx7 = "http://drive.google.com/uc?export=view&id=1EbtQYb4BTFXLvRbkTMQo2XuZCifxEYbK";
-// var supra = "http://drive.google.com/uc?export=view&id=1-O5KPertTVujkFyQo7-KGX253cPDwc6U";
-// var gtr = "http://drive.google.com/uc?export=view&id=1b7FKueTyVW6aUu6W3Dhgmf_68Xuc7Idj";
-// var imperza = "http://drive.google.com/uc?export=view&id=1-TinyPwCIetI8qXXDWEYFMQl1IbWqIeF";
-// var lancer = "http://drive.google.com/uc?export=view&id=1MsPTsHAdxxllo5IokM_572rGv3D474Dt";
-var nsx = "Images/Honda NSX.png";
-var rx7 = "Images/Mazda RX-7.png";
-var supra = "Images/Toyota Supra.png";
-var gtr = "Images/Nissan GT-R.png";
-var imperza = "Images/Subaru Imperza.png";
-var lancer = "Images/Mitsubishi Lancer.png";
+var nsx = "images/Honda NSX.png";
+var rx7 = "images/Mazda RX-7.png";
+var supra = "images/Toyota Supra.png";
+var gtr = "images/Nissan GT-R.png";
+var imperza = "images/Subaru Imperza.png";
+var lancer = "images/Mitsubishi Lancer.png";
+var dealer = "images/map.png";
+var shop = "images/shop.jpeg";
 
 // NAV BAR FUNCTION - SCROLL UP TO HIDE & SCROLL DOWN TO SHOW
 var lastScrollTop = 0;
@@ -29,36 +24,68 @@ window.addEventListener("scroll", function () {
 
 // NAV BAR DROPDOWN (LARGE DISPLAY) - CHANGE CONTENT DYNAMICALLY
 
+var dropdownImage = document.querySelector(".dropdown-image");
+var dropdownCarName = document.querySelector(".dropdown-car-name");
+var dropdownOptionsHeader = document.querySelector(".dropdown-options-header");
+
+var dropdownCarDetailsContainer = document.querySelector(".dropdown-car-details-container");
+var dropdownOptionsDetailsContainer = document.querySelector(".dropdown-options-details-container");
+var dropdownOptionsShopDetails = document.querySelector(".dropdown-options-shop-details");
+var dropdownOptionsDealerDetails = document.querySelector(".dropdown-options-dealer-details");
+
 // Selecting all containers having small car side view image
 var navBarSmallCarModels = document.querySelectorAll(".nav-bar-car-models-container");
 // Adding mouse enter event listener to all the car containers
 for (let i = 0; i < navBarSmallCarModels.length; i++) {
     navBarSmallCarModels[i].addEventListener("mouseenter", function (e) {
-        console.log(this.id);
+        // console.log(this.id);
+        dropdownCarDetailsContainer.style.display = "block"
+        dropdownOptionsDetailsContainer.style.display = "none"
         updateDropdown(this.id)
+    })
+}
+// Selecting dealer and shop links
+var navBarLinks = document.querySelectorAll(".nav-bar-links");
+for (let i=0; i < navBarLinks.length; i++){
+// Adding mouse enter event listener to both links
+    navBarLinks[i].addEventListener("mouseenter", function(e){
+        // console.log(this.id);
+        dropdownCarDetailsContainer.style.display = "none"
+        dropdownOptionsDetailsContainer.style.display = "block"
+        updateDropdown(this.id);
     })
 }
 
 // Function to update the dropdown menu
 function updateDropdown(carName) {
     switch (carName) {
-        case "nsx": document.querySelector(".dropdown-car-img").setAttribute("src", nsx);
-            document.querySelector("#dropdown-car-name").innerHTML = "NSX 2000";
-            break;
-        case "rx7": document.querySelector(".dropdown-car-img").setAttribute("src", rx7);
-            document.querySelector("#dropdown-car-name").innerHTML = "RX-7";
-            break;
-        case "supra": document.querySelector(".dropdown-car-img").setAttribute("src", supra);
-            document.querySelector("#dropdown-car-name").innerHTML = "Supra Mk 4";
-            break;
-        case "gtr": document.querySelector(".dropdown-car-img").setAttribute("src", gtr);
-            document.querySelector("#dropdown-car-name").innerHTML = "R34 GT-R";
-            break;
-        case "imperza": document.querySelector(".dropdown-car-img").setAttribute("src", imperza);
-            document.querySelector("#dropdown-car-name").innerHTML = "Imperza STi";
-            break;
-        case "lancer": document.querySelector(".dropdown-car-img").setAttribute("src", lancer);
-            document.querySelector("#dropdown-car-name").innerHTML = "Lancer Evo IX";
-            break;
+        case "nsx": dropdownImage.setAttribute("src", nsx);
+                    dropdownCarName.innerHTML = "<span class='icon-honda'></span>&ensp;NSX 2000";
+                    break;
+        case "rx7": dropdownImage.setAttribute("src", rx7);
+                    dropdownCarName.innerHTML = "<span class='icon-mazda'></span>&ensp;RX-7";
+                    break;
+        case "supra": dropdownImage.setAttribute("src", supra);
+                    dropdownCarName.innerHTML = "<span class='icon-toyota'></span>&ensp;Supra Mk 4";
+                    break;
+        case "gtr": dropdownImage.setAttribute("src", gtr);
+                    dropdownCarName.innerHTML = "<span class='icon-nissan'></span>&ensp;R34 GT-R";
+                    break;
+        case "imperza": dropdownImage.setAttribute("src", imperza);
+                    dropdownCarName.innerHTML = "<span class='icon-subaru'></span>&ensp;Imperza STi";
+                    break;
+        case "lancer":  dropdownImage.setAttribute("src", lancer);
+                    dropdownCarName.innerHTML = "<span class='icon-mitsubishi'></span>&ensp;Lancer Evo IX";
+                    break;
+        case "dealer" : dropdownOptionsShopDetails.style.display = "none"
+                    dropdownOptionsDealerDetails.style.display = "block"
+                    dropdownImage.setAttribute("src", dealer);
+                    dropdownOptionsHeader.innerHTML = "<a href=''>Contact Us</a>";
+                    break;
+        case "shop" : dropdownOptionsShopDetails.style.display = "block"
+                    dropdownOptionsDealerDetails.style.display = "none"
+                    dropdownImage.setAttribute("src", shop);
+                    dropdownOptionsHeader.innerHTML = "<a href=''>Accessories & Services</a>";
+                    break;
     }
 }
