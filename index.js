d5a16ad70a3fa96a917266b2c18db1a42c700015
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-06-14 20:10:12 +0530

    Completed the Models and Shop section

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var nsx = "images/Honda NSX.png";
 var rx7 = "images/Mazda RX-7.png";
-var supra = "images/Toyota Supra.png";
-var gtr = "images/Nissan GT-R.png";
-var imperza = "images/Subaru Imperza.png";
-var lancer = "images/Mitsubishi Lancer.png";
+var supra = "images/Toyota Supra Mk5.png";
+var gtr = "images/Nissan GT-R35.png";
+var brz = "images/Subaru BRZ.png";
+var lancer = "images/Mitsubishi Lancer Evo X.png";
 var dealer = "images/Dealer Map.png";
 var shop = "images/Shop.png";
 
@@ -33,6 +33,10 @@ var dropdownOptionsDetailsContainer = document.querySelector(".dropdown-options-
 var dropdownOptionsShopDetails = document.querySelector(".dropdown-options-shop-details");
 var dropdownOptionsDealerDetails = document.querySelector(".dropdown-options-dealer-details");
 
+// This is used to hide (display=none) the map
+var hideMap = document.querySelector(".hide-map");
+var hideImage = document.querySelector(".hide-image");
+
 // Selecting all containers having small car side view image
 var navBarSmallCarModels = document.querySelectorAll(".nav-bar-car-models-container");
 // Adding mouse enter event listener to all the car containers
@@ -59,34 +63,53 @@ for (let i=0; i < navBarLinks.length; i++){
 // Function to update the dropdown menu
 function updateDropdown(carName) {
     switch (carName) {
-        case "nsx": dropdownImage.setAttribute("src", nsx);
-                    dropdownCarName.innerHTML = "<span class='icon-honda'></span>&ensp;NSX 2000";
+        case "nsx": hideMap.style.display = "none";
+                    hideImage.style.display = "block";
+                    dropdownImage.setAttribute("src", nsx);
+                    dropdownCarName.innerHTML = "<span class='icon-honda'></span>&ensp;NSX Gen 2";
                     break;
-        case "rx7": dropdownImage.setAttribute("src", rx7);
+        case "rx7": hideMap.style.display = "none";
+                    hideImage.style.display = "block";
+                    dropdownImage.setAttribute("src", rx7);
                     dropdownCarName.innerHTML = "<span class='icon-mazda'></span>&ensp;RX-7";
                     break;
-        case "supra": dropdownImage.setAttribute("src", supra);
-                    dropdownCarName.innerHTML = "<span class='icon-toyota'></span>&ensp;Supra Mk 4";
+        case "supra": hideMap.style.display = "none";
+                    hideImage.style.display = "block";
+                    dropdownImage.setAttribute("src", supra);
+                    dropdownCarName.innerHTML = "<span class='icon-toyota'></span>&ensp;Supra Mk 5";
                     break;
-        case "gtr": dropdownImage.setAttribute("src", gtr);
-                    dropdownCarName.innerHTML = "<span class='icon-nissan'></span>&ensp;R34 GT-R";
+        case "gtr": hideMap.style.display = "none";
+                    hideImage.style.display = "block";
+                    dropdownImage.setAttribute("src", gtr);
+                    dropdownCarName.innerHTML = "<span class='icon-nissan'></span>&ensp;R35 GT-R";
                     break;
-        case "imperza": dropdownImage.setAttribute("src", imperza);
-                    dropdownCarName.innerHTML = "<span class='icon-subaru'></span>&ensp;Imperza STi";
+        case "brz": hideMap.style.display = "none";
+                    hideImage.style.display = "block";
+                    dropdownImage.setAttribute("src", brz);
+                    dropdownCarName.innerHTML = "<span class='icon-subaru'></span>&ensp;BRZ Limited";
                     break;
-        case "lancer":  dropdownImage.setAttribute("src", lancer);
-                    dropdownCarName.innerHTML = "<span class='icon-mitsubishi'></span>&ensp;Lancer Evo IX";
+        case "lancer": hideMap.style.display = "none";
+                    hideImage.style.display = "block";
+                    dropdownImage.setAttribute("src", lancer);
+                    dropdownCarName.innerHTML = "<span class='icon-mitsubishi'></span>&ensp;Lancer Evo X";
                     break;
-        case "dealer" : dropdownOptionsShopDetails.style.display = "none"
+        case "dealer" : hideMap.style.display = "block";
+                    hideImage.style.display = "none";
+                    // TO TOGGLE THE CONTENT OF THE DEALER (FIND US) SECTION
+                    dropdownOptionsShopDetails.style.display = "none"
                     dropdownOptionsDealerDetails.style.display = "block"
-                    dropdownImage.setAttribute("src", dealer);
+                    // dropdownImage.setAttribute("src", dealer);
                     dropdownOptionsHeader.innerHTML = "<a href='mailto:dsumansaurabh@gmail.com'><i class='fa-solid fa-envelope'></i>&ensp;Mail Us</a>";
                     break;
                     
-        case "shop" : dropdownOptionsShopDetails.style.display = "block"
+        case "shop" : hideMap.style.display = "none";
+                    hideImage.style.display = "block";
+                    // TO TOGGLE THE CONTENT OF THE SHOP SECTION
+                    dropdownOptionsShopDetails.style.display = "block"
                     dropdownOptionsDealerDetails.style.display = "none"
                     dropdownImage.setAttribute("src", shop);
-                    dropdownOptionsHeader.innerHTML = "<a href=''>Accessories & Services</a>";
+                    dropdownOptionsHeader.innerHTML = `<a href='#models-section'>Browse Models</a><br>
+                                                       <a href='#shop-section'>Accessories & Services</a>`;
                     break;
     }
 }
